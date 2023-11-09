42c3ef1f921e1e78917518d780a0ec8c67f4bc8e
isthisnull <letscodewithzanjiri@hotmail.com>
2023-11-09 14:03:03 +0300

    80%

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,30 @@ import "./App.css";
 function App() {
   const name = "Space Riders";
   const count = 4;
-  const text = "lorem ipsum";
+  const text =
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore vero  qui aliquid at exercitationem ";
   return (
     <>
       <div className="container">
         <div className="pic">
           <img src="assets/pic.jpg" className="gpAvatar" />
         </div>
-        <div className="group">
-          <p>{name}</p>
-          <p>{count} Members</p>
-        </div>
-        <div className="gpDescription">
-          <p> {text}</p>
-        </div>
-        <div className="actions">
-          <button>Join</button>
-          <div className="shareGroup">
-            <button>Share Group</button>
+        <div className="des">
+          <div className="groupName">
+            <p>{name}</p>
+            <p>{count} Members</p>
+          </div>
+          <div className="gpDescription">
+            <p> {text}</p>
+          </div>
+          <div className="actions">
+            <button className="joinBtn">Join</button>
+            <div className="shareGroup">
+              <button className="shareBtn">
+                <img src="assets/uploadpp.png" className="uploadIcon" />
+                <p>Share Group</p>
+              </button>
+            </div>
           </div>
         </div>
       </div>
