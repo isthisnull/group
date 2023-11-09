0a0e1aa7f4476c201a6630d0c062b014d238243d
isthisnull <letscodewithzanjiri@hotmail.com>
2023-11-09 13:08:11 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,31 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const name = "Space Riders";
+  const count = 4;
+  const text = "lorem ipsum";
+  return (
+    <>
+      <div className="container">
+        <div className="pic">
+          <img src="assets/pic.jpg" className="gpAvatar" />
+        </div>
+        <div className="group">
+          <p>{name}</p>
+          <p>{count} Members</p>
+        </div>
+        <div className="gpDescription">
+          <p> {text}</p>
+        </div>
+        <div className="actions">
+          <button>Join</button>
+          <div className="shareGroup">
+            <button>Share Group</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
